84665677df376b4163d34bcaca6cc480b44d022a
sonicthink <253414082@qq.com>
2016-11-01 09:43:35 +0800

    Update TestOpenApi.java
    init

--- src/main/java/com/wanpucs/openapi/test/TestOpenApi.java
+++ src/main/java/com/wanpucs/openapi/test/TestOpenApi.java
@@ -7,19 +7,19 @@ import com.wanpucs.openapi.exception.OpenApiException;
 import com.wanpucs.openapi.sdk.OpenApi;
 
 /**
- * OpenAPI V3 SDK 示例代码
+ * OpenAPI V1 SDK demo code
  * 
  * @version 3.0.0
- * @since jdk1.5
+ * @since jdk1.7
  * @author open.qq.com
- * @copyright © 2012, Tencent Corporation. All rights reserved.
- * @History: 3.0.0 | nemozhang | 2012-03-21 12:01:05 | initialization
+ * @copyright © 2016,www.wanpucs.com. All rights reserved.
+ * @History:
  * 
  */
 
 public class TestOpenApi {
 	public static void main(String args[]) {
-		// 应用基本信息
+		// basic Key
 		String appkey = "aE7U76KxBe604C9o";
 
 		String secretKey = "uyhXT7CKZ0Q5ZVcJ1V10BFlvMuVa63sG";
@@ -38,7 +38,7 @@ public class TestOpenApi {
 			paramMap.put("type", "main");
 			paramMap.put("status", 1);
 			Object[] paramValues = new Object[] { paramMap };
-			testEchoApi(sdk, methodName, paramValues);
+			testGetMainNavigationTreeApi(sdk, methodName, paramValues);
 		} catch (OpenApiException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,10 +46,11 @@ public class TestOpenApi {
 	}
 
 	/**
-	 * 测试调用UserInfo接口
+	 * test getting navigationTree api
 	 * 
 	 */
-	public static void testEchoApi(OpenApi sdk, String methodName, Object[] args) {
+	public static void testGetMainNavigationTreeApi(OpenApi sdk,
+			String methodName, Object[] args) {
 
 		// 请求方法参数，数组顺序为调用目标方法的参数顺序，类型保持一致，如本例目标方法参数只有一个，参数类型为三个integer类型的参数结构。
 
